5e84262212564969b8704ea7ef8fa5f1d9f1f074
Dominik Ivanoc <dominik.ivanoc@student.tuke.sk>
2023-04-02 09:47:27 +0200

    fixed null REST api and added maven dep

--- src/main/java/APIMaps/server/webService/MapServiceREST.java
+++ src/main/java/APIMaps/server/webService/MapServiceREST.java
@@ -3,16 +3,11 @@ package APIMaps.server.webService;
 
 import APIMaps.entity.FeatureCollection;
 import APIMaps.entity.FeatureCollection2;
-import APIMaps.entity.Geometry2;
 import APIMaps.entity.ReturnTwoDouble;
 import APIMaps.server.service.RouteCalculationService;
 import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.http.ContentDisposition;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,7 +27,7 @@ public class MapServiceREST {
     @Autowired
     private RouteCalculationService routeCalculationService;
 
-    @GetMapping("/route/startX/{startx}/starty/{starty}/endX/{endx}/endY/{endy}")
+    @GetMapping("/route/startX/{startx}/startY/{starty}/endX/{endx}/endY/{endy}")
     public String loadDurationAndDistance(@PathVariable double startx, @PathVariable double starty,
                            @PathVariable double endx, @PathVariable double endy) {
 
@@ -48,12 +43,17 @@ public class MapServiceREST {
     @GetMapping("/route/startX/{startx}/starty/{starty}")
     public double[][][] loadBoundaries(@PathVariable double startx, @PathVariable double starty) {
 
-        FeatureCollection2 result =
-                routeCalculationService.returnBoundaries(startx, starty);
+        if(startx != 0 || starty != 0) {
+            FeatureCollection2 result =
+                    routeCalculationService.returnBoundaries(startx, starty);
 
-        int lastIdx = result.getFeatures().length - 1;
+            int lastIdx = result.getFeatures().length - 1;
 
-        return result.getFeatures()[lastIdx].getGeometry().getCoordinates();
+            return result.getFeatures()[lastIdx].getGeometry().getCoordinates();
+        } else {
+            return new double[0][0][0];
+        }
+
 
     }
 
